22854ab661c920510729c983f91051761212ed11
Hector Rodriguez <hector@hrod.dev>
2023-12-31 08:04:40 -0500

    First Commit Create API Rest in Spring Boot. Create the maker and product entities. Create properties of the connection with DB

---
+++ src/main/java/com/application/rest/entities/Maker.java
@@ -0,0 +1,27 @@
+package com.application.rest.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "maker")
+public class Maker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "maker", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Product> productList = new ArrayList<>();
+}
---
+++ src/main/java/com/application/rest/entities/Product.java
@@ -0,0 +1,28 @@
+package com.application.rest.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_maker", nullable = false)
+    private Maker maker;
+}
